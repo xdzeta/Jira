cacc9c5c6d4b631ad03bae6dd4dcbc0a71cea9a4
Julia Mykhailova <yulia.grischenko@gmail.com>
2015-03-27 15:44:08 +0000

    Laravel Socialite Jira OAuth1 Provider

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# Jira
-SocialiteProviders Jira
+# Laravel Socialite Jira OAuth1 Provider
+
+## Documentation
+
+Full documentation for using this provider can be found at ...)
---
+++ src/JiraExtendSocialite.php
@@ -0,0 +1,17 @@
+<?php
+namespace SocialiteProviders\Jira;
+
+use SocialiteProviders\Manager\SocialiteWasCalled;
+
+class JiraExtendSocialite
+{
+    /**
+     * Execute the provider.
+     */
+    public function handle(SocialiteWasCalled $socialiteWasCalled)
+    {
+        $socialiteWasCalled->extendSocialite(
+            'jira', __NAMESPACE__ . '\Provider', __NAMESPACE__ . '\Server'
+        );
+    }
+}
